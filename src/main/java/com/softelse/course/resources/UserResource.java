98f4ebc90d7dafbc44564c0dc69e1d526e5abc50
renatof4rias <renatof4rias@gmail.com>
2023-12-18 13:32:17 -0300

    User entity e User resource

---
+++ src/main/java/com/softelse/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.softelse.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softelse.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "a", "aa@gmail.com", "999999" , "111123");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+}
