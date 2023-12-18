7a751287e042eac0e86d7e860b5f8b322f121151
renatof4rias <renatof4rias@gmail.com>
2023-12-18 13:05:05 -0300

    setup inicial configurado

---
+++ src/main/java/com/softelse/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.softelse.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
