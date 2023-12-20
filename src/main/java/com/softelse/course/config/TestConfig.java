cdc53d3f3814f24cef206d2a5a5ee86aa4dadaae
renatof4rias <renatof4rias@gmail.com>
2023-12-20 13:20:15 -0300

    Category e Product 90%ok

--- src/main/java/com/softelse/course/config/TestConfig.java
+++ src/main/java/com/softelse/course/config/TestConfig.java
@@ -8,10 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.softelse.course.entities.Category;
 import com.softelse.course.entities.Order;
+import com.softelse.course.entities.Product;
 import com.softelse.course.entities.User;
 import com.softelse.course.entities.enums.OrderStatus;
+import com.softelse.course.repositories.CategoryRepository;
 import com.softelse.course.repositories.OrderRepository;
+import com.softelse.course.repositories.ProductRepository;
 import com.softelse.course.repositories.UserRepository;
 
 @Configuration
@@ -24,6 +28,12 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired	
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -34,8 +44,20 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
+		Category c1 = new Category (null, "Sobremesa");
+		Category c2 = new Category (null, "Entrada");
+		Category c3 = new Category (null, "Prato Principal");
+		
+		Product p1 = new Product (null, "Panqueca", "Prato Sobremesa", 7.50, "http:aaaaa.com");
+		Product p2 = new Product (null, "Picanha", "Prato Principal", 77.50, "http:aaaaa.com");
+		Product p3 = new Product (null, "Batata Frita", "Prato Entrada", 7.50, "http:aaaaa.com");
+		
+		
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 	
---
+++ src/main/java/com/softelse/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.softelse.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.softelse.course.entities.Product;
+import com.softelse.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
