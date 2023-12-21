07003492b9441e55819ad3d729916dd13ec60476
renatof4rias <renatof4rias@gmail.com>
2023-12-21 10:59:32 -0300

    Class OrderItem Finaly

--- src/main/java/com/softelse/course/config/TestConfig.java
+++ src/main/java/com/softelse/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.softelse.course.entities.Category;
 import com.softelse.course.entities.Order;
+import com.softelse.course.entities.OrderItem;
 import com.softelse.course.entities.Product;
 import com.softelse.course.entities.User;
 import com.softelse.course.entities.enums.OrderStatus;
 import com.softelse.course.repositories.CategoryRepository;
+import com.softelse.course.repositories.OrderItemRepository;
 import com.softelse.course.repositories.OrderRepository;
 import com.softelse.course.repositories.ProductRepository;
 import com.softelse.course.repositories.UserRepository;
@@ -33,7 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
-	
+
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -56,7 +60,6 @@ public class TestConfig implements CommandLineRunner{
 		Product p5 = new Product (null, "Hamburgue", "Prato Principal", 47.50, "http:aaaaa.com");
 		Product p6 = new Product (null, "Cebola Crisp", "Prato Entrada", 17.50, "http:aaaaa.com");
 		
-		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
@@ -72,6 +75,14 @@ public class TestConfig implements CommandLineRunner{
 		p6.getCategories().add(c2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4,p5, p6));
+		
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2 , p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1 , p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2 , p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2 , p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 	
--- src/main/java/com/softelse/course/entities/OrderItem.java
+++ src/main/java/com/softelse/course/entities/OrderItem.java
@@ -3,6 +3,7 @@ package com.softelse.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.softelse.course.entities.pk.OrderItemPK;
 
 import jakarta.persistence.EmbeddedId;
@@ -15,7 +16,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -32,6 +33,7 @@ public class OrderItem implements Serializable{
 		id.setProduct(product);
 	}
 
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
